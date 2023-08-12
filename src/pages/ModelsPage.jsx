2675271055d745e849dde22e8fbcf5a5bc5377fc
JeanPiton <jean.orpiton@gmail.com>
2023-08-12 14:41:19 -0300

    Feature: filter race

--- src/pages/ModelsPage.jsx
+++ src/pages/ModelsPage.jsx
@@ -6,12 +6,16 @@ import PageController from "../components/PageController"
 
 export default function ModelsPage(){
     const [models,setModels] = useState([])
-    const [search,setSearch] = useState({search:"",status:""})
+    const [search,setSearch] = useState({search:"",status:"",race:""})
     const [update,setUpdate] = useState(false)
+    const [races,setRaces] = useState([])
     const page = new URLSearchParams(location.search).get("page")
 
     useEffect(()=>{
-        axios.get(`${import.meta.env.VITE_API_URL}/models/?offset=${isNaN(page)?0:page*10}&search=${search.search}&status=${search.status}`)
+        axios.get(`${import.meta.env.VITE_API_URL}/models/races`)
+        .then(resp=>setRaces(resp.data))
+        .catch(error=>console.log(error.message))
+        axios.get(`${import.meta.env.VITE_API_URL}/models/?offset=${isNaN(page)?0:page*10}&search=${search.search}&status=${search.status}&race=${search.race}`)
         .then(resp=>setModels(resp.data))
         .catch(error=>console.log(error.message))
     },[update])
@@ -26,10 +30,15 @@ export default function ModelsPage(){
             <SearchDiv>
                 <form onSubmit={e=>Search(e)}>
                     <input type="text" onChange={e=>setSearch(previous=>({...previous, ['search']:e.target.value}))} value={search.search}/> 
+                    <button>pesquisar</button>
                     <select onChange={e=>setSearch(previous=>({...previous, ['status']:e.target.value}))} value={search.status}>
-                        <option value="">Todos</option>
+                        <option value="">Status</option>
                         <option value={true}>Disponiveis</option>
                         <option value={false}>Indisponiveis</option>
+                    </select>
+                    <select onChange={e=>setSearch(previous=>({...previous, ['race']:e.target.value}))} value={search.race}>
+                        <option value="">Raças</option>
+                        {races.map(e=><option value={e.id}>{e.name}</option>)}
                     </select>
                 </form>
             </SearchDiv>
